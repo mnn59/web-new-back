d1726fcccae1180787c5c3ca18533ebb654107db
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-20 11:41:01 -0700

    Add some config to User

--- models/user.go
+++ models/user.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	Id uint
-	Name string
-	FamilyName string
-	Email string `gorm:"unique"`
-	Password []byte
-	Address string
-	ChargeAmount string
+	Id           uint   `json:"id"`
+	Name         string `json:"name"`
+	FamilyName   string `json:"family_name"`
+	Email        string `json:"email" gorm:"unique"`
+	Password     []byte `json:"-"`
+	Address      string `json:"address"`
+	ChargeAmount string `json:"charge_amount"`
 }
